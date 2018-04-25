42b5f86219524c727c4c9b229f11938877bf7045
Fábio Henrique <fabiosjc@gmail.com>
2018-04-24 21:07:50 -0300

    Aula 3: Enviando dados para o WebService

--- 01_rest-assured/leiloes-rest-assured/src/br/com/caelum/leilao/teste/LeilaoWSTest.java
+++ 01_rest-assured/leiloes-rest-assured/src/br/com/caelum/leilao/teste/LeilaoWSTest.java
@@ -2,7 +2,10 @@ package br.com.caelum.leilao.teste;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+import com.jayway.restassured.path.xml.XmlPath;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.jayway.restassured.path.json.JsonPath;
@@ -11,8 +14,8 @@ import br.com.caelum.leilao.modelo.Leilao;
 import br.com.caelum.leilao.modelo.Usuario;
 
 public class LeilaoWSTest {
-	
-	@Test
+    
+    @Test
 	public void deveRetornarLeilaoPeloId() {
 		JsonPath path = given()
 				.parameter("leilao.id", 1)
@@ -37,6 +40,40 @@ public class LeilaoWSTest {
 				.andReturn().jsonPath();
 		
 		assertEquals(path.getInt("int"), 2);		
-	} 
+	}
+
+    @Test
+    public void deveAdicionarUmNovoLeilao() {
+        Usuario mauricio = new Usuario(1l, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+        Leilao novoLeilao = new Leilao(100l, "Leilão de Teste", 500.0, mauricio, false);
+
+        XmlPath jsonPath = given()
+                    .header("Accept", "application/xml")
+                    .contentType("application/xml")
+                    .body(novoLeilao)
+                .expect()
+                    .statusCode(200)
+                .when()
+                    .post("/leiloes")
+                .andReturn()
+                    .xmlPath();
+
+        Leilao leilao = jsonPath.getObject("leiloes", Leilao.class);
+
+        assertEquals(leilao, novoLeilao);
+        assertEquals(leilao.getUsuario(), mauricio);
+
+        given()
+                    .header("Accept", "application/xml")
+                    .contentType("application/xml")
+                    .body(leilao)
+                .expect()
+                    .statusCode(200)
+                .when()
+                    .delete("/leiloes/deletar")
+                .andReturn()
+                    .asString();
+
+    }
 
 }
