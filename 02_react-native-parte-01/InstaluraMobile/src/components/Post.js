7335b7bb0563043d67e041bbdfaa4c6c4b2fd7d1
Fábio Henrique <fsiqueira@atech.com.br>
2018-05-14 16:42:38 -0300

    04-Implementando as curtidas

--- 02_react-native-parte-01/InstaluraMobile/src/components/Post.js
+++ 02_react-native-parte-01/InstaluraMobile/src/components/Post.js
@@ -4,41 +4,79 @@ import {
   Text,
   View,
   Dimensions,
-  Image
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      foto: this.props.foto
+    }
+  }
+
+  carregaIcone(likeada) {
+    return likeada ? require('../../resources/img/s2-checked.png') :
+      require('../../resources/img/s2.png')
+  }
+
+  like() {
+    const fotoAtualizada = {
+      ...this.state.foto,
+      likeada: !this.state.foto.likeada
+    }
+    this.setState({ foto: fotoAtualizada });
+  }
+
   render() {
+    const { foto } = this.state;
     return (
-        <View>
-          <View style={styles.cabecalho}>
-              <Image source={{uri: this.props.foto.urlPerfil}}
-                      style={styles.fotoDePerfil} />
-              <Text>{this.props.foto.loginUsuario}</Text>
-          </View>
-          <Image source={{uri: this.props.foto.urlFoto}}
-                style={styles.foto} />        
+      <View>
+        <View style={styles.cabecalho}>
+          <Image source={{ uri: foto.urlPerfil }}
+            style={styles.fotoDePerfil} />
+          <Text>{foto.loginUsuario}</Text>
         </View>
+
+        <Image source={{ uri: foto.urlFoto }}
+          style={styles.foto} />
+
+        <View style={styles.rodape}>
+          <TouchableOpacity onPress={this.like.bind(this)}>
+            <Image style={styles.botaoDeLike}
+              source={this.carregaIcone(foto.likeada)} />
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   cabecalho: {
-      margin: 10,
-      flexDirection: 'row',
-      alignItems: 'center',
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   fotoDePerfil: {
-      marginRight: 10,
-      borderRadius: 20,
-      width: 40,
-      height: 40,
+    marginRight: 10,
+    borderRadius: 20,
+    width: 40,
+    height: 40,
   },
   foto: {
-      width: width,
-      height: width,
+    width: width,
+    height: width,
   },
+  rodape: {
+    margin: 10,
+  },
+  botaoDeLike: {
+    height: 40,
+    width: 40,
+  }
 });
